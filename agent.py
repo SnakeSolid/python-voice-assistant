9dc2249e11ade7810be38e50553ddae71e0753f3
Shabanov Anton <iamshabanov@yandex.ru>
2024-01-27 20:04:02 +0300

    fixed action in agent example

--- agent.py
+++ agent.py
@@ -46,26 +46,28 @@ if __name__ == "__main__":
         print("Таймер на {}:{:02}".format(minutes, seconds))
 
     def alarm(hours, minutes):
-        print("Будильник на {}:{:02}:00".format(hours, minutes))
+        print("Будильник на {}:{:02}".format(hours, minutes))
+
+    def start(name):
+        print("Найти `{}`".format(name))
 
     def search(query):
-        print("Найти `{}`".format(query))
+        print("Запустить `{}`".format(query))
 
     agent = Agent()
     agent.action("(поставь|заведи|добавь|создай) таймер на # секунд",
                  lambda s: timer(0, s))
     agent.action("(поставь|заведи|добавь|создай) таймер на # минут",
                  lambda m: timer(m, 0))
-    agent.action("(поставь|заведи|добавь|создай) будильник на # #",
+    agent.action("(поставь|заведи|добавь|создай) будильник на # часов # минут",
                  lambda h, m: alarm(h, m))
     agent.action("(поставь|заведи|добавь|создай) будильник на # часов",
                  lambda h: alarm(h, 0))
     agent.action("(поставь|заведи|добавь|создай) будильник на # минут",
                  lambda m: alarm(-1, m))
-    agent.action("(поставь|заведи|добавь|создай) будильник на # часов # минут",
+    agent.action("(поставь|заведи|добавь|создай) будильник на # #",
                  lambda h, m: alarm(h, m))
-    agent.action("(открой|запусти) будильник на # часов # минут",
-                 lambda h, m: alarm(h, m))
+    agent.action("(открой|запусти) *", lambda n: start(n))
     agent.action("(найди|поищи) ...", lambda q: search(q))
 
     LOGGER.info("Ready.")
